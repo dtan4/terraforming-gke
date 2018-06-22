71ec2dbe3211239ca522f2535812db305d057331
Daisuke Fujita <dtanshi45@gmail.com>
2018-06-22 12:31:55 +0900

    Remove unnecesarry spec

--- spec/terraforming/gke_spec.rb
+++ spec/terraforming/gke_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Terraforming::GKE do
   it "has a version number" do
     expect(Terraforming::GKE::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
